b8132d90b0efb90ea52e0aa294ebd41834b1aee2
Emrick19 <emricky17@gmail.com>
2020-08-12 20:50:04 +0100

    Remove unnecessary comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); //for env file to load up
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -6,8 +6,9 @@ const cors = require('cors');
 
 
 const app = express();
-// allow my application to be consumed
+app.use(express.json()); 
 app.use(cors());
+
 
 //Import routes
 const registerRoute = require("./routes/register");
@@ -26,14 +27,12 @@ mongoose
     console.log(err);
   });
 
-// Middlewares
-app.use(express.json()); //replacing bodyParser
 
 //routes midddlewares
-app.use("/api/user", registerRoute); //url example = api/user/register
+app.use("/api/user", registerRoute); 
 app.use("/api/user", loginRoute);
 app.use("/api/secrets", secretsRoute);
-// app.use('/api/secrets', postRoute)
+
 let port = 8080 || process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on port ${port}....`));
